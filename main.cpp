c06232e70f1eb40ac903c5c6bfe4e47015295797
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-26 02:55:33 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//Hoppock, Zachary
+//talkToHal.cpp
+//9/12/2020
+//Introduce yourself to Hal
+//Version #1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+string name;
+
+  cout << "Hello my name is Hal" << endl;
+  cout << "What is your name?" << endl;
+  cin >> name;
+
+  cout << "Hello, " << name << " I am glad to meet you.";
+  return 0;
+}
